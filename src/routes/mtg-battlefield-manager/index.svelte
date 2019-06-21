07cabc6b4c218c6bef841c6f9edf095cd76e71d5
Lyndogwin <brand949@gmail.com>
2019-06-21 14:41:19 -0400

    added mtg route

---
+++ src/routes/mtg-battlefield-manager/index.svelte
@@ -0,0 +1,11 @@
+<script>
+    let name = "Player1"
+    let score = "100"
+</script>
+
+<div class = "container">
+    <div class = "card">
+        <h1>{name}</h1>
+        <h3>{points}</h3>
+    </div>
+</div>
---
+++ src/routes/mtg/index.svelte
@@ -0,0 +1,15 @@
+<script>
+    let name = "Player1"
+    let life = 20
+    const addLife = () => (life += 1)
+    const subLife = () => (life -= 1)
+</script>
+
+<div class="container">
+    <div class="card">
+        <h1>{name}</h1>
+        <h3>{life}</h3>
+        <button class="btn" on:click={addLife}>+1</button>
+        <button class="btn" on:click={subLife}>-1</button>
+    </div>
+</div>
